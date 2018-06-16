6c24c50119c8d559182b17536a8e549046f98ec8
Anant Jain <anantjain60@gmail.com>
2018-06-16 22:59:51 +0530

    Migrations Added
    Removed sample migrations and added new migrations

---
+++ database/migrations/2018_06_16_165419_add_foregion_key.php
@@ -0,0 +1,88 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForegionKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('event', function (Blueprint $table) {
+            $table->index('creator');
+            $table->index('subid');
+            $table->foreign('creator')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('subid')->references('id')->on('subject')->onDelete('cascade');
+        });
+
+        Schema::table('option', function (Blueprint $table) {
+            $table->index('queid');
+            $table->foreign('queid')->references('id')->on('queans')->onDelete('cascade');
+        });
+
+        Schema::table('queans', function (Blueprint $table) {
+            $table->index('eventid');
+            $table->foreign('eventid')->references('id')->on('event')->onDelete('cascade');
+        });
+
+        Schema::table('request', function (Blueprint $table) {
+            $table->index('userid');
+            $table->index('eventid');
+            $table->foreign('userid')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('eventid')->references('id')->on('event')->onDelete('cascade');
+        });
+
+        Schema::table('response', function (Blueprint $table) {
+            $table->index('userid');
+            $table->index('quesid');
+            $table->foreign('userid')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('quesid')->references('id')->on('queans')->onDelete('cascade');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event', function (Blueprint $table) {
+            $table->dropForeign('creator');
+            $table->dropForeign('subid');
+            $table->dropIndex('creator');
+            $table->dropIndex('subid');
+        });
+
+        Schema::table('option', function (Blueprint $table) {
+            $table->dropForeign('queid');
+            $table->dropIndex('queid');
+        });
+
+        Schema::table('queans', function (Blueprint $table) {
+            $table->dropForeign('eventid');
+            $table->dropIndex('eventid');
+        });
+
+        Schema::table('request', function (Blueprint $table) {
+            $table->dropForeign('userid');
+            $table->dropForeign('eventid');
+            $table->dropIndex('userid');
+            $table->dropIndex('eventid');
+        });
+
+        Schema::table('response', function (Blueprint $table) {
+            $table->dropForeign('userid');
+            $table->dropForeign('quesid');
+            $table->dropIndex('userid');
+            $table->dropIndex('quesid');
+        });
+        
+    }
+}
