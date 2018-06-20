a47607d13f4079dd658a25dd807e18027e0f1f52
Anant Jain <anantjain60@gmail.com>
2018-06-21 01:11:55 +0530

    Removed Confict

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
+Route::group(['middleware' => ['web']], function ()
+{
+	Route::get('/', function ()
+	{
+    	return view('home');
+	})->name('login');
+
+	Route::post('login', 'InfoConnectApiController@login')->name('LoginUrl');
+
+	Route::get('/logout', 'InfoConnectApiController@logout')->name('LogoutinUrl');
+
+	Route::group(['middleware' => ['auth']], function ()
+	{
+
+		Route::group(['prefix' => '/student'], function()
+		{
+			Route::get('/', function ()
+			{
+		    	//return view('home');
+		    	dd("Student Dashboard");
+			});
+		});
+
+		Route::group(['prefix' => '/teacher'], function()
+		{
+			Route::get('/', function ()
+			{
+		    	//return view('home');
+		    	dd("Teacher Dashboard");
+			});
+		});
+
+		Route::group(['prefix' => '/society'], function()
+		{
+			Route::get('/', function ()
+			{
+		    	return view('student/home');
+		    	// dd("Society Dashboard");
+			});
+		});
+
+	});
 });
