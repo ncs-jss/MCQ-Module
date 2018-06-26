5c6835c9ce22f71156a7a5583da0a35e5fd6318c
Anant Jain <anantjain60@gmail.com>
2018-06-26 23:06:00 +0530

    Event Page

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -24,12 +24,40 @@ body {
 				@if ($i==1 || $i==4 || $i==7)
     			<div class="card-deck">
     			@endif
-  					<div class="card animated pulse shadow">
+  					<div class="card 
+  					@if ($event->start > date('Y-m-d H:i:s') && $event->end > date('Y-m-d H:i:s'))
+  						text-white bg-warning
+  					@elseif ($event->end < date('Y-m-d H:i:s'))
+  						text-white bg-dark
+  					@else
+  						text-white bg-success
+  					@endif
+  					 animated pulse shadow">
+      					<div class="card-header">
+                  <h3>{{ $event->name }}</h3>
+                </div>
     					<div class="card-body">
-      						<h5 class="card-title">{{ $event->name }}</h5>
-      						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      						<a href="student/event/{{ $event->id }}" class="btn btn-primary">Start Quiz</a>
+      						<table class="table table-borderless table-sm">
+                    <tbody>
+      							<tr>
+      								<td>Start: </td><td> {{ date('d M Y, g:i a',strtotime($event->start)) }}</td>
+      							</tr>
+      							<tr>
+      								<td>Close: </td><td> {{ date('d M Y, g:i a',strtotime($event->end)) }}</td>
+      							</tr>
+      							<tr>
+      								<td>Duration: </td><td> {{ sprintf("%02d",intdiv($event->duration, 60)).':'. sprintf("%02d",($event->duration % 60)) }}</td>
+      							</tr>
+                  </tbody>
+      						</table>
     					</div>
+                  @if ($event->start <= date('Y-m-d H:i:s') && $event->end > date('Y-m-d H:i:s'))
+                    <a href="student/event/{{ $event->id }}"><div class="bg-primary card-footer"><center><strong class="text-white">Start Quiz</strong></center></div></a>
+                  @elseif ($event->start > date('Y-m-d H:i:s'))
+                    <div class="bg-danger card-footer"><center><strong class="text-white">Comming Soon</strong></center></div>
+                  @else
+                    <div class="card-footer"><center><strong class="text-white">Closed</strong></center></div>
+                  @endif
   					</div>
 				@if ($i==3 || $i==6 || $i==9)
 				</div>
