9e04e6fe395da0b03b400edcb6547a8aba5f7ec4
Anant Jain <anantjain60@gmail.com>
2018-10-01 23:32:27 +0530

    Fixed CSS

--- app/Helpers/custom_url.php
+++ app/Helpers/custom_url.php
@@ -2,6 +2,6 @@
 function custom_url($arg){
     if(substr($arg, 0, 1 ) != "/")
         $arg = "/".$arg;
-    return "/laravel/test2/Proj_mx02/public".$arg;
+    return "/proj_mx02/public".$arg;
 }
 ?>
