7d5497ec630fc1639a7a06102651d34a84cbb335
Anant Jain <anantjain60@gmail.com>
2019-01-23 20:06:06 +0530

    Fix PSR

--- app/Event.php
+++ app/Event.php
@@ -9,21 +9,43 @@ class Event extends Model
     protected $table = 'event';
     public $timestamps = false;
 
+    /**
+     * Many event created to one user relation.
+     *
+     * @return $this
+     */
     public function user()
     {
         return $this->belongsTo('App\User', 'creator');
     }
 
+    /**
+     * Many event join requeest to one user relation.
+     *
+     * @return $this
+     */
     public function userRequest()
     {
-        return $this->belongsToMany('App\User', 'req', 'eventid', 'userid')->withPivot('status');
+        return $this->belongsToMany('App\User', 'req', 'eventid', 'userid')
+            ->withPivot('status');
     }
 
+    /**
+     * Many event to one subject relation.
+     *
+     * @return $this
+     */
     public function subject()
     {
         return $this->belongsTo('App\Subject', 'subid');
     }
 
+
+    /**
+     * One event to many question relation.
+     *
+     * @return $this
+     */
     public function queans()
     {
         return $this->hasMany('App\Queans', 'eventid');
